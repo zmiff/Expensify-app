139c43432833d2d56a08dcb38712fe2d3ca95670
Ugur Tamer <ugur_tamer@hotmail.com>
2019-01-13 21:25:33 +0100

    fixed misssing gitignore

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,17 +3,19 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import configureStore from '../store/configureStore';
 import selectExpenses from '../selectors/expenses';
-import SelectExpenesTotal from '../selectors/expenses-total';
+import selectExpenesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 
-export const ExpensesSummary = ({expensesCount, expensesTotal}) => {
+export const ExpensesSummary = ({expensesCount, expensesTotal, totalExpensesCount, totalExpensesTotal}) => {
   const expenseWord = expensesCount === 1 ? 'expense' : 'expenses';
-  const formattedExpenseTotal = numeral(expensesTotal / 100).format('0,0.00')+'kr '
-
+  const formattedExpenseTotal = numeral(expensesTotal / 100).format('0,0.00')+'kr ';
+  const totalExpenseWord = totalExpensesCount === 1 ? 'expense' : 'expenses';
+  const formattedTotalExpenseTotal = numeral(totalExpensesTotal / 100).format('0,0.00')+'kr ';
   return (
     <div className="page-header">
       <div className="content-container">
         <h1 className="page-header__title">Vieving <span>{expensesCount}</span> {expenseWord} totaling: <span>{formattedExpenseTotal}</span></h1>
+        <p className="page-header__under--title">Total {totalExpensesCount} {totalExpenseWord} totaling:{formattedTotalExpenseTotal}</p>
         <div className="page-header__actions">
           <Link to="/create" className="button">Add Expense</Link>
         </div>
@@ -23,10 +25,13 @@ export const ExpensesSummary = ({expensesCount, expensesTotal}) => {
 }
 
 const mapStateToProps = (state) => {
-    let expenses = selectExpenses(state.expenses, state.filters)
+    let expenses = selectExpenses(state.expenses, state.filters);
+    let totalExpenses = selectExpenses(state.expenses, {text: '',sortBy: 'date',startDate: 0, endDate: 2547410201} )
   return {
     expensesCount: expenses.length,
-    expensesTotal: SelectExpenesTotal(expenses)
+    totalExpensesCount: totalExpenses.length,
+    expensesTotal: selectExpenesTotal(expenses),
+    totalExpensesTotal: selectExpenesTotal(totalExpenses)
   };
 }
 
